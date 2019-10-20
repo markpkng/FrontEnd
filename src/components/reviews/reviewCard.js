5f90e2088329a91ee516922fd6704a7899ae5ab2
Mark King <markpkng@gmail.com>
2019-10-20 00:52:35 -0400

    review stars and login radio buttons

--- src/components/reviews/reviewCard.js
+++ src/components/reviews/reviewCard.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import ReactStars from 'react-rating-stars-component';
+import styled from 'styled-components';
+
+const ReviewDiv = styled.div `
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
 
 const ReviewCard = ({review}) => {
     const {stars, comment, date, reviewer, id} = review;
     return (
-        <div>
+        <ReviewDiv>
             <p>ID: {id}</p>
-            <p>Stars: {stars}</p>
+            <ReactStars count={5} value={stars} edit={false} size={50} color2={'#ffd700'}/>
             <p>Comment: {comment}</p>
             <p>Date: {date}</p>
             <p>Reviewer: {reviewer}</p>
-        </div>
+        </ReviewDiv>
     );
 }
 
--- src/components/reviews/reviewForm.js
+++ src/components/reviews/reviewForm.js
@@ -1,26 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
+import ReactStars from 'react-rating-stars-component';
 
 const FlexColumn = styled.div `
     display: flex;
     flex-direction: column;
-    width: 500px;
+    align-items: center;
     margin: 0 auto;
 `
 
 const ReviewForm = () => {
+    const [rating, setRating] = useState(5);
     return (
         <div>
             <form>
                 <FlexColumn>
                     <h2>Write A Review:</h2>
-                    <label>Stars: <select>
-                        <option value={1}>1</option>
-                        <option value={2}>2</option>
-                        <option value={3}>3</option>
-                        <option value={4}>4</option>
-                        <option value={5}>5</option>
-                    </select></label>
+                    <ReactStars count={5} value={rating} onChange={value => setRating(value)} size={50} color2={'#ffd700'}/>
                     <input type='text' placeholder='Comment'/>
                     <label>Post as anonymous? <input type='checkbox'/></label>
                     <button type='submit'>Submit</button>
